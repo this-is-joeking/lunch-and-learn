ecb0996935dea6637a6a27fdd04e0ff4d07166a1
Joe King <j.king92@gmail.com>
2023-03-06 08:55:34 -0700

    test: CountryFacade

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,7 +1,12 @@
 class CountryFacade
   def self.random
     countries_data = CountryService.random
-    random_num = rand(1..countries_data.count)
-    countries_data[random_num][:name][:common]
+    countries_data[random_num(countries_data.count)][:name][:common]
+  end
+
+  private
+  
+  def self.random_num(max)
+    rand(1..max)
   end
 end
