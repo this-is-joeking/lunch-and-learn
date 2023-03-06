ee0a8d73c915d9e40cc803e4af91e89d0c682560
Joe King <j.king92@gmail.com>
2023-03-06 09:13:38 -0700

    test: Country service

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,11 +1,11 @@
 class CountryFacade
   def self.random
-    countries_data = CountryService.random
+    countries_data = CountryService.all
     countries_data[random_num(countries_data.count)][:name][:common]
   end
 
   private
-  
+
   def self.random_num(max)
     rand(1..max)
   end
--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -1,5 +1,5 @@
 class CountryService
-  def self.random
+  def self.all
     response = Faraday.get('https://restcountries.com/v3.1/all')
     JSON.parse(response.body, symbolize_names: true)
   end
