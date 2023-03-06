173418c7e92d5303ebdcbbb8cca19d4d31387b77
Joe King <j.king92@gmail.com>
2023-03-06 08:10:01 -0700

    fix spacing

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
-  class RecipesController < ApplicationController
-  def index
-    recipes = RecipeFacade.by_country(params[:country])
-    render json: RecipeSerializer.new(recipes), status: :ok
-  end
-  end
+    class RecipesController < ApplicationController
+      def index
+        recipes = RecipeFacade.by_country(params[:country])
+        render json: RecipeSerializer.new(recipes), status: :ok
+      end
+    end
   end
 end
