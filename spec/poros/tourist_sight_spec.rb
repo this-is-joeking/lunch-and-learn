beab2659bacf922c2ae5187e3d55859f3d8a8b89
Joe King <j.king92@gmail.com>
2023-03-06 11:30:27 -0700

    test: TOurist SIght poro test added

---
+++ spec/poros/tourist_sight_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe TouristSight do
+  it 'exists with attributes' do
+    sight_data = {:type=>"Feature",
+      :properties=>
+       {:name=>"Palais du Louvre",
+        :formatted=>"Louvre Palace, Avenue du Général Lemonnier, 75001 Paris, France",
+        :place_id=>"518d1bce0ed2af02405927f526f2356e4840f00101f90159c731000000000092031050616c616973206475204c6f75767265"}}
+    
+    sight = TouristSight.new(sight_data)
+
+    expect(sight).to be_a TouristSight
+    expect(sight.id).to eq nil
+    expect(sight.name).to eq('Palais du Louvre')
+    expect(sight.address).to eq('Louvre Palace, Avenue du Général Lemonnier, 75001 Paris, France')
+    expect(sight.place_id).to eq('518d1bce0ed2af02405927f526f2356e4840f00101f90159c731000000000092031050616c616973206475204c6f75767265')
+  end
+end
