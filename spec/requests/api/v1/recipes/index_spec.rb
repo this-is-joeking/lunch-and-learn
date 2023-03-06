ea4b08d2de0a20337dd5d9281b5423ec719897d2
Joe King <j.king92@gmail.com>
2023-03-05 17:37:40 -0700

    test: Add fixtures and tests for when country param is empty and when country param yields no results

--- spec/requests/api/v1/recipes/index_spec.rb
+++ spec/requests/api/v1/recipes/index_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'get recipes by country' do
   it 'returns an array of recipes for the given country' do
-    stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=9c81b1a0&app_key=#{ENV['recipe_api_key']}&q=italy&type=any")
+    country = "italy"
+
+    stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=9c81b1a0&app_key=#{ENV['recipe_api_key']}&q=#{country}&type=any")
       .to_return(status: 200, body: File.read('spec/fixtures/recipes_italy.json'), headers: {})
     
-    country = "italy"
 
     get "/api/v1/recipes?country=#{country}"
 
@@ -29,8 +30,38 @@ RSpec.describe 'get recipes by country' do
     end
   end
   
-  xit 'returns an empty array if country param is empty or no results' do
+  it 'returns an empty array if country param is empty or no results' do
+    country = ""
 
+    stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=9c81b1a0&app_key=#{ENV['recipe_api_key']}&q=#{country}&type=any")
+      .to_return(status: 200, body: File.read('spec/fixtures/recipes_empty_query.json'), headers: {})
+    
+    get "/api/v1/recipes?country=#{country}"
+
+    empty_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(empty_response).to be_a Hash
+    expect(empty_response.keys).to eq([:data])
+    expect(empty_response[:data]).to eq([])
+  end
+
+  it 'returns an empty array if there are no results' do
+    country = "Kyrgyzstan"
+
+    stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=9c81b1a0&app_key=#{ENV['recipe_api_key']}&q=#{country}&type=any")
+      .to_return(status: 200, body: File.read('spec/fixtures/recipes_kyrgyzstan.json'), headers: {})
+
+    get "/api/v1/recipes?country=#{country}"
+
+    empty_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(empty_response).to be_a Hash
+    expect(empty_response.keys).to eq([:data])
+    expect(empty_response[:data]).to eq([])
   end
 
   xit 'gets recipes for a random country if country param is missing' do
