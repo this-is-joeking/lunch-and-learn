392bcfd54d214963756db27c5aa914fd8e32ae18
Joe King <j.king92@gmail.com>
2023-03-07 10:19:31 -0700

    Add route and test for creating a user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :recipes, only: :index
       resource :learning_resources, only: :show
+      resources :users, only: :create
     end
   end
 end
---
+++ spec/requests/api/v1/users/create_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'creating a new user' do
+  it 'receives a post request, creates user, sends back api key' do
+    name = Faker::TvShows::BojackHorseman.character
+    email = Faker::Internet.email(name: name)
+
+    headers = {
+      'CONTENT_TYPE' => 'application/json',
+      'ACCEPT' => 'application/json'
+    }
+    body = {
+      name: name,
+      email: email
+    }
+    require 'pry'; binding.pry
+    post '/api/v1/users', headers: headers, body: body.to_json, as: :json
+
+    new_user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response).to have_http_status(201)
+    expect(new_user).to be_a Hash
+    expect(new_user.keys).to eq(:data)
+    expect(new_user[:data]).to be_a Hash
+    expect(new_user[:data].keys.sort).to eq([:type, :id, :attributes].sort)
+    expect(new_user[:data][:type]).to eq('user')
+    expect(new_user[:data][:id]).to eq('1')
+    expect(new_user[:data][:attributes]).to be_a Hash
+    expect(new_user[:data][:attributes].keys.sort).to eq([:name, :email, :api_key].sort)
+    expect(new_user[:data][:attributes][:name]).to eq(name)
+    expect(new_user[:data][:attributes][:email]).to eq(email)
+    expect(new_user[:data][:attributes][:api_key]).to be_a String
+    # TODO: once api key generation is place consider more thorough testing of the key value
+  end
+
+  xit 'sends a error response if user with email already exists' do
+
+
+    error_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response).to have_http_status(422)
+    expect(error_data).to be_a Hash
+    expect(error_data.keys).to eq([:error])
+    expect(error_data[:error].keys.sort).to eq([:code, :message])
+    expect(error_data[:error][:code]).to eq(422)
+    expect(error_data[:error][:message]).to eq('Email already exists for another user')
+  end
+end
