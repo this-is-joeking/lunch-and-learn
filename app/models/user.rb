dfe4a79874ea4b4a9afa52eee4180d372a1bb861
Joe King <j.king92@gmail.com>
2023-03-07 14:48:10 -0700

    User model with api created prior to creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,16 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :email
   validates_uniqueness_of :email, case_sensitive: false
+  validates_uniqueness_of :api_key
 
-  after_validation :create_api_key
+  before_create :create_api_key
 
   private 
 
   def create_api_key
-    self.api_key = SecureRandom.urlsafe_base64(32).tr('-_','')
-    # User.find_by(api_key: api_key)
-    # require 'pry'; binding.pry
-  end
-
-  def insure_uniqueness(key)
-    # need to check if key is unique
-    # if it is unique assign it to the user
-    # if it is not generate new key and try again
-    if User.find_by(api_key: key).nil?
+    loop do
+      self.api_key = SecureRandom.urlsafe_base64(32).tr('-_','')
+      break unless User.find_by(api_key: self.api_key)
     end
   end
 end
