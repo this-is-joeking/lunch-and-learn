41a605f09369ecb6f39926ba9963c5d4158dafca
Joe King <j.king92@gmail.com>
2023-03-07 17:17:28 -0700

    linting

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -1,6 +1,6 @@
 class RecipeFacade
   def self.by_country(country)
-    country = random_country unless country
+    country ||= random_country
 
     recipes_data = RecipeService.by_country(country)
     recipes_data[:hits].map do |recipe_data|
